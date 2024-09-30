f8707c8c112a51a78c35467cc7baa60d21590e44
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-10-01 01:34:15 +0200

    started the project, initial routing and layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from '@/components/Menu'
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* Left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-gray-100 p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+        <Image src="/logo.png" alt="logo" width={32} height={32}/>
+        <span className="hidden lg:block">Sunlight</span>
+        </Link>
+        <Menu/>
+      </div>
+      {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-gray-200">Right</div>
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParentPage = () => {
+  return (
+    <div>Parent Page</div>
+  )
+}
+
+export default ParentPage
